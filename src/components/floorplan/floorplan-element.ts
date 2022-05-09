8ec81f8dab6912f61367e40218f92ef4446f2eac
Yangqian <yanyangqian@gmail.com>
2022-05-09 12:32:48 +0800

    match style

--- src/components/floorplan/floorplan-element.ts
+++ src/components/floorplan/floorplan-element.ts
@@ -1017,6 +1017,11 @@ export class FloorplanElement extends LitElement {
           rule.hold_action === undefined
             ? defaultRule.hold_action
             : rule.hold_action;
+            
+        rule.hover_info_filter =
+          rule.hover_info_filter === undefined
+            ? defaultRule.hover_info_filter
+            : rule.hover_info_filter;
       }
     }
 
@@ -1479,13 +1484,12 @@ export class FloorplanElement extends LitElement {
   handleEntitySetHoverOver(entityInfo: FloorplanEntityInfo): void {
     const entityId = entityInfo.entityId as string;
     const entityState = this.hass.states[entityId];
-    const filter = this.config.defaults.hover_info_filter;
 
     for (const ruleInfo of entityInfo.ruleInfos) {
       if (ruleInfo.rule.hover_action) {
         let isHoverInfo =
           typeof ruleInfo.rule.hover_action === 'string' &&
-          ruleInfo.rule.hover_action === 'hover-info';
+          ruleInfo.rule.hover_action === 'hover-info'; 
         isHoverInfo =
           isHoverInfo ||
           (typeof ruleInfo.rule.hover_action === 'object' &&
@@ -1499,6 +1503,8 @@ export class FloorplanElement extends LitElement {
             ));
 
         if (isHoverInfo) {
+          const hoverInfoFilter = new Set<string>(ruleInfo.rule.hover_info_filter);
+
           for (const svgElementInfo of Object.values(
             ruleInfo.svgElementInfos
           )) {
@@ -1520,7 +1526,7 @@ export class FloorplanElement extends LitElement {
                 titleText += `State: ${entityState.state}\n\n`;
 
                 Object.keys(entityState.attributes).map((key) => {
-                  if !(filter && filter.includes(key)){
+                  if !(hoverInfoFilter.has(key)){
                     titleText += `${key}: ${
                       (entityState.attributes as Record<string, unknown>)[key]
                     }\n`;
