314d34769e1b0eca2c98dfce266066e09bda7fc9
Petar Kozul <Petar.Kozul@finity.com.au>
2022-11-24 12:42:24 +1100

    Use browser locale for entity last changed/updated

--- src/components/floorplan/lib/date-util.ts
+++ src/components/floorplan/lib/date-util.ts
@@ -1,7 +1,22 @@
 import strftime from 'strftime';
-import { format as timeago } from 'timeago.js';
+import TimeAgo from 'javascript-time-ago';
+
+import * as files from '../../../../node_modules/javascript-time-ago/locale/*.js';
+
+let locale = navigator.language; // use browser's locale
+if (!files[`${locale}.json`]) { // check if locale supported by TimeAgo package
+  locale = locale.includes('-') ? locale.split('-')[0] : locale; // if not, try shorter version of locale
+}
+locale = files[`${locale}.json`] ? locale : 'en'; // if locale not supported by TimeAgo package, use English (en) locale
+
+TimeAgo.addDefaultLocale(files[`${locale}.json`].default);
+const timeAgo = new TimeAgo(navigator.language);
 
 export class DateUtil {
   static strftime = strftime;
-  static timeago = timeago;
+
+  static timeago(date: string | Date) {
+    const targetDate = (typeof date === 'string') ? new Date(date) : date;
+    return timeAgo.format(targetDate, 'round');
+  }
 }
