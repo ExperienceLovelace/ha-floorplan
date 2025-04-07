61aaa3a1f6e159f31ff6adf2215dd93e26ee0828
exetico <tobiasnordahl@gmail.com>
2025-04-07 19:21:39 +0200

    Format doc

--- tests/jest/jest-common-utils.ts
+++ tests/jest/jest-common-utils.ts
@@ -1,5 +1,5 @@
-
-export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * Retry a function until it succeeds or the maximum number of retries is reached.
@@ -9,7 +9,11 @@ export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve
  * @returns The result of the function if it succeeds.
  * @throws An error if the function does not succeed within the maximum retries.
  */
-export const retry = async <T>(fn: () => Promise<T>, retries: number, delay: number): Promise<T> => {
+export const retry = async <T>(
+  fn: () => Promise<T>,
+  retries: number,
+  delay: number
+): Promise<T> => {
   let attempt = 0;
   while (attempt < retries) {
     try {
