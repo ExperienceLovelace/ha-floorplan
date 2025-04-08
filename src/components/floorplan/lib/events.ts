69c718bbd02ccbf2df004d1cb8cf3adc798224cf
exetico <tobiasnordahl@gmail.com>
2025-04-08 13:50:31 +0200

    Allow SVGElement as el-type

--- src/components/floorplan/lib/events.ts
+++ src/components/floorplan/lib/events.ts
@@ -1,7 +1,10 @@
 // src/lib/events.ts
 export const HA_FLOORPLAN_ACTION_CALL_EVENT = 'ha-floorplan-service-call';
 
-export function dispatchFloorplanActionCallEvent(el: SVGGraphicsElement, detail: any) {
+export function dispatchFloorplanActionCallEvent(
+  el: SVGElement | SVGGraphicsElement,
+  detail: any
+) {
   const event = new CustomEvent(HA_FLOORPLAN_ACTION_CALL_EVENT, {
     detail,
     bubbles: true,
