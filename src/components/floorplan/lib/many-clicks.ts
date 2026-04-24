b636f2e396a5627239f8173529c07649b7908a09
MrBearPresident <jerrelatruwe@hotmail.com>
2026-04-24 09:43:36 +0200

    Prevent long touch while scrolling:  - move touch part from many-clicks to long-clikcs  - Test

--- src/components/floorplan/lib/many-clicks.ts
+++ src/components/floorplan/lib/many-clicks.ts
@@ -12,22 +12,12 @@ export class ManyClicks {
     elements.add(elem);
 
     const doubleClickDuration = 400;
-    const touchMoveThreshold = 10; // pixels
 
     let timer: ReturnType<typeof setTimeout>;
 
     let clickCount = 0;
-    let touchStartX = 0;
-    let touchStartY = 0;
-    let isValidClick = true;
 
     const onClickEvent = () => {
-      // Only proceed if this is a valid click (not from a swipe/scroll)
-      if (!isValidClick) {
-        isValidClick = true;
-        return;
-      }
-
       clickCount++;
 
       timer = setTimeout(() => {
@@ -44,23 +34,9 @@ export class ManyClicks {
       }
     };
 
-    const onTouchStart = (e: TouchEvent) => {
-      touchStartX = e.touches[0].clientX;
-      touchStartY = e.touches[0].clientY;
-      isValidClick = true;
-    };
 
-    const onTouchMove = (e: TouchEvent) => {
-      const deltaX = Math.abs(e.touches[0].clientX - touchStartX);
-      const deltaY = Math.abs(e.touches[0].clientY - touchStartY);
 
-      if (deltaX > touchMoveThreshold || deltaY > touchMoveThreshold) {
-        isValidClick = false;
-      }
-    };
 
-    E.on(elem, 'touchstart', onTouchStart.bind(this));
-    E.on(elem, 'touchmove', onTouchMove.bind(this));
     E.on(elem, 'click', onClickEvent.bind(this));
   }
 }
